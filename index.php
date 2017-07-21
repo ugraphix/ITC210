9b9928019b6eeffa2278461f80e761106f0493e4
Jaewon Jeong <7904001@gmail.com>
2017-07-21 12:49:09 -0700

    Loop

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-// Silence is golden.
+/**
+ * The main template file
+ */
+
+get_header(); ?>
+
+<h1><?php the_title(); ?></h1>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer();
