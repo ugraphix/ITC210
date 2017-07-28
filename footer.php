7dd9f8fe009057b65454e1455f0191cf006c46e9
Kristinmahyout <Kydelv@gmail.com>
2017-07-28 10:10:18 -0700

    template files + visual design studies

---
+++ footer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "off-canvas-wrap" div and all content after.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+?>
+
+		</section>
+		<div class="footer-container" data-sticky-footer>
+			<footer class="footer">
+				<?php do_action( 'foundationpress_before_footer' ); ?>
+				<?php dynamic_sidebar( 'footer-widgets' ); ?>
+				<?php do_action( 'foundationpress_after_footer' ); ?>
+			</footer>
+		</div>
+
+		<?php do_action( 'foundationpress_layout_end' ); ?>
+
+<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+		</div><!-- Close off-canvas content -->
+	</div><!-- Close off-canvas wrapper -->
+<?php endif; ?>
+
+
+<?php wp_footer(); ?>
+<?php do_action( 'foundationpress_before_closing_body' ); ?>
+</body>
+</html>
